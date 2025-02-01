3c51fd9c86b2b80e5199aaf5cf436ae6705013ce
LgCookie <lgc2333@126.com>
2025-02-01 15:35:55 +0800

    up

--- scripts/pull.py
+++ scripts/pull.py
@@ -7,10 +7,16 @@ if TYPE_CHECKING:
 
 
 def main():
-    paths = [*iter_modules(tip=False)]
-    while paths:
-        failed_paths: list[Path] = []
+    max_retry = 3
 
+    paths = [*iter_modules(tip=False)]
+    for i in range(1, max_retry + 1):
+        if not paths:
+            break
+        if i > 1:
+            print(f"Starting retry failed operations ({i} / {max_retry})")
+
+        failed_paths: list[ Path] = []
         for p in paths:
             tip_entering(p)
             if not ok(system("git", "pull", cwd=str(p), check=False)):
@@ -18,7 +24,9 @@ def main():
                 failed_paths.append(p)
 
         paths = failed_paths
-        if paths:
-            print("Starting retry failed operations")
+
+    else:
+        print(f"Still have failed operations after {max_retry} tries, stopping")
+        return
 
     system("git", "pull")
